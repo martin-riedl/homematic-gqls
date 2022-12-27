181aa8302ff5c34fdd5af5b4b5524656abe5c41f
Jasmin Riedl <jasmin.riedl@posteo.de>
2022-12-27 14:14:08 +0100

    minor changes

--- model.py
+++ model.py
@@ -3,7 +3,8 @@
 from ariadne import QueryType
 import homematicip
 from homematicip.home import Home
-from homematicip.device import ShutterContact, TemperatureHumiditySensorDisplay
+from homematicip.device import Device, ShutterContact, TemperatureHumiditySensorDisplay
+from typing import List
 
 # load homematic config
 config = homematicip.find_and_load_config_file()
@@ -23,20 +24,24 @@ home.enable_events()
 
 query = QueryType()
 
-@query.field("shuttercontacts")
-def resolve_ShutterContact(_, info):
+@query.field("genericdevices")
+def resolve_Device(_, info) -> List[Device]:
+    return [d for d in home.devices]
+
+@query.field("shuttercontacts") 
+def resolve_ShutterContact(_, info) -> List[ShutterContact]:
     return [d for d in home.devices if isinstance(d, ShutterContact)]
 
 @query.field("shuttercontacts_filtered")
-def resolve_ShutterContact_Filtered(*_, label_filter=""):
+def resolve_ShutterContact_Filtered(*_, label_filter="") -> List[ShutterContact]:
     return [d for d in home.devices if isinstance(d, ShutterContact) if label_filter in d.label ]
 
 @query.field("ths_displays")
-def resolve_TemperatureHumiditySensorDisplay(_, info):
+def resolve_TemperatureHumiditySensorDisplay(_, info) -> List[TemperatureHumiditySensorDisplay]:
     return [d for d in home.devices if isinstance(d, TemperatureHumiditySensorDisplay)]
 
 @query.field("ths_displays_filtered")
-def resolve_TemperatureHumiditySensorDisplay(*_, label_filter=""):
+def resolve_TemperatureHumiditySensorDisplay(*_, label_filter="") -> List[TemperatureHumiditySensorDisplay]:
     return [d for d in home.devices if isinstance(d, TemperatureHumiditySensorDisplay) if label_filter in d.label ]
 
 
